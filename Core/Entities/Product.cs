940c99f4ba415db8c8c266f21ff1ef80d5a606e1
NDuc <nguyenngocduc260504@gmail.com>
2025-05-08 17:30:53 +0700

    complete settup code base

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Core.Entities;
+
+public class Product : BaseEntities
+{
+    public required string ProductName { get; set; }
+    public required string Descriptions { get; set; }
+    public decimal Price { get; set; }
+    public required string ImageUrl { get; set; }
+    public required string Type { get; set; }
+    public required string Brand { get; set; }
+    public int QuantityInStock { get; set; }
+
+}
