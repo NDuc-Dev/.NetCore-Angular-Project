17508386cfa488ea952274ebbd4c42749dd0848a
NDuc <nguyenngocduc260504@gmail.com>
2025-05-20 16:40:16 +0700

    create product models. pagination and using for appcomponent

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: number;
+    productName: string;
+    descriptions: string;
+    imageUrl: string;
+    type: string;
+    price: number;
+    quantityInStock: number;
+    brand: string;
+}
