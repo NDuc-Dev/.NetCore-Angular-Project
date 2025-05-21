18baff091f91fdcb8e17f040cf1acd795fc8be29
NDuc <nguyenngocduc260504@gmail.com>
2025-05-21 17:57:11 +0700

    added getBrands, getTypes method

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -7,10 +7,25 @@ import { Product } from '../../shared/models/product';
   providedIn: 'root'
 })
 export class ShopService {
-    baseUrl = 'https://localhost:5001/api/'
+  baseUrl = 'https://localhost:5001/api/'
   private http = inject(HttpClient);
+  type: String[] = [];
+  brand: String[] = [];
 
-  getProducts(){
-    return this.http.get<Pagination<Product>>(this.baseUrl+'products?pagesize=20')
+  getProducts() {
+    return this.http.get<Pagination<Product>>(this.baseUrl + 'products?pagesize=20')
+  }
+
+  getBrands() {
+    if (this.brand.length > 0) return this.brand;
+    return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe({
+      next: response => this.brand = response,
+    })
+  }
+  geTypes() {
+    if (this.type.length > 0) return this.type;
+    return this.http.get<string[]>(this.baseUrl + 'products/type').subscribe({
+      next: response => this.type = response,
+    })
   }
 }
