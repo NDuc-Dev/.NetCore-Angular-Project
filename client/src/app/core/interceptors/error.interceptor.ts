bffd0e95c2b4296d4be7d2482270748c2b9cd270
NDuc <nguyenngocduc260504@gmail.com>
2025-05-24 00:41:33 +0700

    added  loading interceptor, busy services to create loading status, update header fixed

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -1,25 +1,39 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
+import { SnackbarService } from '../services/snackbar.service';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
-
+  const snackbar = inject(SnackbarService);
 
   return next(req).pipe(
-    catchError((err : HttpErrorResponse) => {
+    catchError((err: HttpErrorResponse) => {
       if (err.status === 400) {
-        alert(err.error.title || err.error);
+        if (err.error.errors) {
+          const modelStateError = [];
+          for (const key in err.error.errors) {
+            if (err.error.errors[key]) {
+              modelStateError.push(err.error.errors[key])
+            }
+          }
+          throw modelStateError.flat()
+        }
+        else {
+          snackbar.error(err.error.title || err.error);
+
+        }
       }
       if (err.status === 401) {
-        alert(err.error.title || err.error);
+        snackbar.error(err.error.title || err.error);
       }
       if (err.status === 404) {
         router.navigateByUrl('/not-found');
       }
       if (err.status === 500) {
-        router.navigateByUrl('/server-error');
+        const navigationExtras: NavigationExtras = {state : {error: err.error}}
+        router.navigateByUrl('/server-error', navigationExtras);
       }
       throw throwError(() => err);
     })
