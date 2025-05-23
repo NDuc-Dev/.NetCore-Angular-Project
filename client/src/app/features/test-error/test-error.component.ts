0344408affe3bd6bce05d2539cb9ac985a87cec3
NDuc <nguyenngocduc260504@gmail.com>
2025-05-24 00:40:14 +0700

    added snackbar service for handle error on page, update not found page, internal server error page

--- client/src/app/features/test-error/test-error.component.ts
+++ client/src/app/features/test-error/test-error.component.ts
@@ -13,6 +13,8 @@ import { MatButton } from '@angular/material/button';
 export class TestErrorComponent {
   baseUrl = 'https://localhost:5001/api/';
   private http = inject(HttpClient);
+  validationErrors?: string[];
+
 
   get404Error() {
     this.http.get(this.baseUrl + 'buggy/notfound').subscribe({
@@ -35,13 +37,14 @@ export class TestErrorComponent {
   get400ValidationError() {
     this.http.post(this.baseUrl + 'buggy/validationerror', {}).subscribe({
       next: response => console.log(response),
-      error: error => console.log(error)
+      error: error => this.validationErrors = error
     })
   }
   get400Error() {
     this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
       next: response => console.log(response),
       error: error => console.log(error)
+      
     })
   }
 
