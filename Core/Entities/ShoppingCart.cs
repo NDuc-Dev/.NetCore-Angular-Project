5d62f8c609a5fba6a976397d2fe65f0bce2337e4
NDuc <nguyenngocduc260504@gmail.com>
2025-05-24 18:17:44 +0700

    Create Cart using redis

---
+++ Core/Entities/ShoppingCart.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Diagnostics.SymbolStore;
+
+namespace Core.Entities;
+
+public class ShoppingCart
+{
+    public required string Id { get; set; }
+    public List<CartItem> Items { get; set; } = [];
+}
