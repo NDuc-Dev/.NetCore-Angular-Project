521b0bf093e5afd5168bb7293336e86562ae4ede
NDuc <nguyenngocduc260504@gmail.com>
2025-06-03 17:40:30 +0700

    Add identity, update db and add custom register endpoint, testing authen, create extension method for claimprinciple and addressmapping, add endpoint to update user address

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
@@ -26,6 +27,9 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
     return ConnectionMultiplexer.Connect(configuration);
 });
 builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<StoreContext>();
 
 var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
@@ -36,6 +40,7 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
 
 
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<AppUser>();
 
 try
 {
